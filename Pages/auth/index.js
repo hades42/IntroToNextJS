dc85536b07bfbe2072216c060987e8e4e2fb65f0
hades42 <nguyenvannguyen.oc@gmail.com>
2021-04-04 23:07:24 +1000

    adding User component to the /auth route

--- Pages/auth/index.js
+++ Pages/auth/index.js
@@ -1,7 +1,10 @@
 import React from "react";
+
+import User from "../../Component/User";
 const indexPage = () => (
     <div>
         <h1>The Auth Index Page</h1>
+        <User name="Van Nguyen" age={24}></User>
     </div>
 )
 
